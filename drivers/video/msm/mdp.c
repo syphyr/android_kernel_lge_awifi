735d3097863508d8fcaa87dcc01a27c956176398
Ethan Chen <intervigil@gmail.com>
2015-04-24 23:56:39 -0700

    msm: mdp: Add support for display always on mode
    * Add a new panel power state to represent a panel low power mode.
    
    Change-Id: Ie307012ddd62f9690b99b44c3efd9cac6895beb1
    
    Conflicts:
    	drivers/video/msm/mdp.c
    	drivers/video/msm/mdp4_overlay.c
    	drivers/video/msm/msm_fb.c
    	drivers/video/msm/msm_fb_panel.h
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay.c

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -1182,7 +1182,7 @@ int mdp_histogram_stop(struct fb_info *info, uint32_t block)
 
 	mgmt->mdp_is_hist_start = FALSE;
 
-	if (!mfd->panel_power_on) {
+	if (mdp_fb_is_power_off(mfd)) {
 		if (mgmt->hist != NULL) {
 			mgmt->hist = NULL;
 			complete(&mgmt->mdp_hist_comp);
@@ -2411,7 +2411,12 @@ static int mdp_off(struct platform_device *pdev)
 	int ret = 0;
 	struct msm_fb_data_type *mfd = platform_get_drvdata(pdev);
 
-	pr_info("%s:+\n", __func__);
+	pr_debug("%s:+\n", __func__);
+	if (mdp_fb_is_power_on_lp(mfd)) {
+		pr_debug("panel not turned off. keeping overlay on\n");
+		ret = panel_next_low_power_config(pdev, true);
+		return ret;
+	}
 	mdp_histogram_ctrl_all(FALSE);
 	atomic_set(&vsync_cntrl.suspend, 1);
 	atomic_set(&vsync_cntrl.vsync_resume, 0);
@@ -2492,6 +2497,8 @@ static int mdp_on(struct platform_device *pdev)
 		mdp_iommu_max_map_size = mfd->max_map_size;
 
 #ifndef CONFIG_MACH_LGE
+	ret = panel_next_low_power_config(pdev, false);
+
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
 
 	ret = panel_next_on(pdev);
